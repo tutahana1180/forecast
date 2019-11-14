789aaaeca24ca906247a38d0f1b38cdf1ab6e749
tutahana1180 <tutahana1180@gmail.com>
2019-11-14 02:51:12 +0000

    final

---
+++ html/resources/webapp/controller/algorithms/pal/auto_arima.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+	"forecast/html/base/algorithms/Controller"
+], function(Controller) {
+	"use strict";
+	return Controller.extend("forecast.html.controller.algorithms.pal.auto_arima", {
+		forcedSelectedAlgorithm: "auto_arima",
+		onPressExecute: function(oEvent) {
+			Controller.prototype.onPressExecute.apply(this, oEvent);
+				this.setVizProperties("result_fit_viz_frame", "result_fit_popover");			
+		}
+	});
+});
